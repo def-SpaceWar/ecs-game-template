3c8ceea3cfb70058b8f16f29ed4b7996b273c13a
Aryan Ahire <aryanahire9086360780@gmail.com>
2023-07-04 16:37:14 -0400

    added some code for animation, it's not done yet

--- src/lib/components/animation_controller.ts
+++ src/lib/components/animation_controller.ts
@@ -1,9 +1,10 @@
-// TODO
 import { Component, Entity } from "../ecs";
+import { AnimationData } from "../util/animation";
 
 export class AnimationController implements Component {
     constructor(
         public entity: Entity,
-        // public animations: Animation[]
+        public animations: AnimationData[],
+        public currentAnimation = animations[0].name
     ) {}
 }
--- src/lib/util/animation.ts
+++ src/lib/util/animation.ts
@@ -1,15 +1,44 @@
-// TODO
 import { AnimationController } from "../components/animation_controller";
+import { Component } from "../ecs";
 
-// Static, Relative
-export class AnimationType {}
+export class PropertyMap<T extends Component, K extends keyof T> {
+    constructor(
+        public readonly Target: new(...args: any[]) => T,
+        public readonly key: K,
+        public readonly value: T[K]
+    ) { }
+}
 
-// Holds all the animation data
-export class Animation {
-    constructor() {}
+export class Keyframe<T extends Component> {
+    constructor(
+        public readonly Targets: (new(...args: any[]) => T)[],
+        public readonly propertyMap: PropertyMap<T, keyof T>[],
+        public readonly timeLength: number,
+        public readonly isRelative = false,
+        public readonly isSmooth = false
+    ) { }
+}
 
-    static setAnimation(animationController: AnimationController, animationId: string) {
-        animationController;
-        animationId;
+export class AnimationData {
+    currentKeyframe: number;
+    currentKeyframeLength: number;
+
+    constructor(
+        public readonly name: string,
+        public readonly keyframes: Keyframe<Component>[]
+    ) {
+        this.currentKeyframe = 0;
+        this.currentKeyframeLength = keyframes[0].timeLength;
+    }
+}
+
+export namespace AnimationManager {
+    export function setAnimation(animationController: AnimationController, animationName: string) {
+        for (const animation of animationController.animations) {
+            if (animation.name != animationName) continue;
+            animation.currentKeyframe = 0;
+            animation.currentKeyframeLength = animation.keyframes[0].timeLength;
+        }
+        animationController.currentAnimation = animationName;
     }
 }
